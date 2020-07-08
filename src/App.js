f7bbfade138f45795c67473f70bacc27b74d3bd2
Wallace Holanda <wallace.holanda@hotmail.com>
2020-07-07 21:44:13 -0300

    :sparkles: Creating the application project and designing the layout

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+import Header from './components/Header';
+import Board from './components/Board';
+import List from './components/List';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Board />
+
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
